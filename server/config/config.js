5a0dba0224bb24f386e8b699c8b7c9ceebea830c
Emanuel Vargas <silverzero55@gmail.com>
2018-12-05 22:05:16 -0600

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 // =============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =============================
+// ENTORNO
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =============================
+// BASE DE DATOS
+// =============================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:noire55@ds249428.mlab.com:49428/cafe';
+}
+
+process.env.URLDB = urlDB;
